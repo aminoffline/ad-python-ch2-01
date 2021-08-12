2a5eea9906bcda6c01b2392b46e4140ff9ede732
Amin <aminoffline@yahoo.com>
2021-08-12 14:49:33 +0430

    it works

--- main.py
+++ main.py
@@ -16,11 +16,7 @@ class ClassRoom:
         ClassRoom.cc =+ 1
     def buildclass(self,student_number,Lage,Lheight,Lweight):
         self.number = student_number
-        #self.l_age , self.l_height , self.l_weight = Lage , Lheight ,Lweight
-        self.l_age = Lage
-        self.l_height = Lheight
-        self.l_weight = Lweight
-
+        self.l_age , self.l_height , self.l_weight = Lage , Lheight ,Lweight
         classs =[Student(a, h, w) for a, h, w in zip(self.l_age, self.l_height, self.l_weight)]
         return classs
 
@@ -30,55 +26,50 @@ class ClassRoom:
         for i in self:
             sum_a.append(i.age)
             sum_h.append(i.height)
-            sum_w.append((i.weight))
+            sum_w.append(i.weight)
         avg['avg_a'] = sum(sum_a)/len(sum_a)
         avg['avg_h'] = sum(sum_h)/len(sum_h)
         avg['avg_w'] = sum(sum_w)/len(sum_w)
         return avg
-        #return f'{sum_a}\n{sum_h}\n{sum_w}'
 
     def h_comper(self,other):
         if ClassRoom.avg(self)['avg_h'] > ClassRoom.avg(other)['avg_h']:
-            print(self)
+            print(f'A')
         elif ClassRoom.avg(self)['avg_h'] == ClassRoom.avg(other)['avg_h']:
             if ClassRoom.avg(self)['avg_w'] >= ClassRoom.avg(other)['avg_w']:
-                print(f"{self}")
+                print(f"A")
             else:
-                print(f'{other}')
+                print(f'B')
         else:
-            print(f'{other}')
+            print(f'B')
 
 
-"""
-n = int(input())
+CA = ClassRoom()
+CB = ClassRoom()
 
+n1 = int(input())
 l_age = input().split(' ')
 l_hieght = input().split(' ')
 l_weight = input().split(' ')
 l_age = [float(i) for i in l_age]
 l_hieght = [float(i) for i in l_hieght]
 l_weight = [float(i) for i in l_weight]
-"""
-l_age = [16,17, 15, 16, 17]
-l_hieght = [180,175,172,170,165]
-l_weight =  [67,72,59,62,55]
 
-CA = ClassRoom()
-CB = ClassRoom()
-A = CA.buildclass(5, l_age,l_hieght,l_weight)
-l_age = [15,16, 15, 16, 17]
-l_hieght = [160,175,162,170,165]
-l_weight =  [57,72,55,62,55]
-B = CB.buildclass(5, l_age,l_hieght,l_weight)
-#print(ClassRoom.avg(cr))
+A = CA.buildclass(n1, l_age,l_hieght,l_weight)
+
+n2 = int(input())
+l_age = input().split(' ')
+l_hieght = input().split(' ')
+l_weight = input().split(' ')
+l_age = [float(i) for i in l_age]
+l_hieght = [float(i) for i in l_hieght]
+l_weight = [float(i) for i in l_weight]
+
+B = CB.buildclass(n2, l_age,l_hieght,l_weight)
+
 for i,j in ClassRoom.avg(A).items():
     print(j ,sep="\n")
 for i,j in ClassRoom.avg(B).items():
     print(j ,sep="\n")
 
 ClassRoom.h_comper(A,B)
-
-
-#print( list(i.age for i in cr))
-
-
